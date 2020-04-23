3b6f40c4a0b4d9e51811432ccaa6f60a63368479
jirou.tanaka <tanakajiroumaru12@gmail.com>
2020-04-23 04:03:06 +0000

    top page

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,35 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    public function index()
+    {
+        $keyword = '';
+    $response = null;
+
+    $url = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706';
+    
+    // applicationIdの 'xxxxx....' は取得したアプリIDに書き換える
+    $params = [
+        'format' => 'json',
+        'applicationId' => '1084521773457364307',
+        'hits' => 15,
+        'imageFlag' => 1
+    ];
+    if (array_key_exists('keyword', $_POST)) {
+        $keyword = $_POST['keyword'];
+        $response_json = execute_api($url, $params, $keyword);
+        $response = json_decode($response_json);
+        
+        return view('welcome');
+    }
+    
+    function execute_api($url, $params, $keyword) {
+        $query = http_build_query($params, "", "&");
+        $search_url = $url . '?' . $query . '&keyword=' . $keyword;
+        
+        return file_get_contents($search_url);
+    
+    }
+    }
 }
