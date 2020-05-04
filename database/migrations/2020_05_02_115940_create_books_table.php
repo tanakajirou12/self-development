ce29e8e79b2e14c7a9125fad7fd68c0e6ef10355
jirou.tanaka <tanakajiroumaru12@gmail.com>
2020-05-04 05:41:31 +0000

    1

--- database/migrations/2020_05_02_115940_create_books_table.php
+++ database/migrations/2020_05_02_115940_create_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSearchesTable extends Migration
+class CreateBooksTable extends Migration
 {
     /**
      * Run the migrations.
